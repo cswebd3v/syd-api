6f722d978a779b7f6c8f1546f21e3e0b2ff28411
Chris Swenson <cswebd3v@gmail.com>
2020-04-21 10:43:32 -0500

    data update and testing

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,51 @@
 const app = require('../src/app')
+const knex = require('knex');
 
-describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
-    return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!')
+describe('app endpoints', () => {
+  let db;
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DATABASE_URL
+    });
+    app.set('db', db);
   })
-})
+
+  describe('GET /faces', () => {
+
+    it('GET /faces responds with 200', () => {
+      return supertest(app)
+        .get('/api/faces')
+        .expect(200)
+    })
+  });
+
+  describe('GET /faces/genders/:gender', () => {
+
+    it('GET /faces/genders/:gender responds with 200', () => {
+      return supertest(app)
+        .get('/api/faces/genders/:gender')
+        .expect(200)
+    })
+  });
+
+  describe('GET /faces/ages/:age', () => {
+
+    it('GET /faces/ages/:age responds with 200', () => {
+      return supertest(app)
+        .get('/api/faces/ages/:age')
+        .expect(200)
+    })
+  });
+
+  describe('GET /both/:gender/:age', () => {
+
+    it('GET /both/:gender/:age responds with 200', () => {
+      return supertest(app)
+        .get('/api/both/:gender/:age')
+        .expect(200)
+    })
+  });
+
+});
